48da5776eb56c6d319ec54a8fb79c087e01a4a1f
Murs000 <129412232+Murs000@users.noreply.github.com>
2024-09-23 15:14:28 +0400

    App. Core. Domain. Add FilePack Interface.

--- Core/CitizenFileManagement.Core.Application/obj/Debug/net8.0/CitizenFileManagement.Core.Application.AssemblyInfo.cs
+++ Core/CitizenFileManagement.Core.Application/obj/Debug/net8.0/CitizenFileManagement.Core.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CitizenFileManagement.Core.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffd144e08939e9af51d7ea40c12a84b1bcd4e7d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92a675e809ad90d09bb7fab873337683712c95a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CitizenFileManagement.Core.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CitizenFileManagement.Core.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
