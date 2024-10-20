23deb76308b11df208f4c2f196ec98a50f5f5b7c
Murs000 <129412232+Murs000@users.noreply.github.com>
2024-10-20 18:02:59 +0400

    App. Infrasructure. External. Add MinIO Settings.

---
+++ Infrastructure/CitizenFileManagement.Infrastructure.External/Settings/MinIOSettings.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace CitizenFileManagement.Infrastructure.External.Settings;
+
+public class MinIOSettings
+{
+    public string Endpoint { get; set; }
+    public string AccessKey { get; set; }
+    public string SecretKey { get; set; }
+}
