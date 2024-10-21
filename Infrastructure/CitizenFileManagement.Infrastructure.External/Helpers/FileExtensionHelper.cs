8425b5306f1b2158f4604e748b7a5fcfe7032b5a
Murs000 <129412232+Murs000@users.noreply.github.com>
2024-10-21 11:51:27 +0400

    App. Infrastrucutre. External. Refactor FileExtencionHelper.

--- Infrastructure/CitizenFileManagement.Infrastructure.External/Helpers/FileExtensionHelper.cs
+++ Infrastructure/CitizenFileManagement.Infrastructure.External/Helpers/FileExtensionHelper.cs
@@ -4,7 +4,7 @@ namespace CitizenFileManagement.Infrastructure.External.Helpers;
 
 public static class FileExtensionHelper
 {
-    public static string GetExtension(string fileExtension)
+    public static string GetContentType(string fileExtension)
     {
         return fileExtension switch
         {
@@ -14,6 +14,18 @@ public static class FileExtensionHelper
             ".png" => "image/png",
             // Add more file types as needed
             _ => "application/octet-stream"
+        };
+    }
+    public static string GetExtension(string fileExtension)
+    {
+        return fileExtension switch
+        {
+            "application/pdf" => ".pdf",
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document" => ".docx",
+            "image/jpeg" => ".jpg",
+            "image/png" => ".png",
+            // Add more file types as needed
+            _ => ".txt"
         };
     }
 }
